aacd37a18b8f27958803c681985cd94e1843f41c
HopeMorehead <splendidwonder@gmail.com>
2017-07-06 10:11:03 -0400

    Practice start with for loops

--- LoopsPractice1/Program.cs
+++ LoopsPractice1/Program.cs
@@ -10,6 +10,39 @@ namespace LoopsPractice1
     {
         static void Main(string[] args)
         {
+            //When would you use a loop?
+            //Why would you use a loop?
+            //Nested Loops
+            //Infinite Loops
+            //Keywords break and continue
+            //for loops
+            //foreachloops
+            //while loops
+            //do-while loops
+
+
+            //For Loop
+            //Used for both counting and 
+            //Iterating through an array (or other collection type)
+            //Looks like this
+            //for(initializer;condition;updater)
+            //  {
+
+            //    do something
+            //}
+
+
+            //    for(int i=0; i <= 10; i++)
+            //   {
+            //      Console.WriteLine(i);
+            //     }
+            //  prints the 0-11 all that meets the condition.
+
+            for (int i = 15; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+           
         }
     }
 }
